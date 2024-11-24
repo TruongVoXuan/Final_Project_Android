f6ed05abab0fb8b830a400dec6b1f21c7ed7508d
TruongVoXuan <truong.75.cvan@gmail.com>
2024-11-24 14:14:25 +0700

    UI_main(90%)

---
+++ WheyShop/app/src/main/java/truong/vx/wheyshop/SimilarAdapter.java
@@ -0,0 +1,64 @@
+package truong.vx.wheyshop;
+
+import android.annotation.SuppressLint;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class SimilarAdapter extends RecyclerView.Adapter<SimilarAdapter.MyViewHolder> {
+
+    private List<BestDeal> bestDealList;
+    public  interface OnMyItemCickListener{
+        void DoSomeThing (int position);
+    }
+    private OnMyItemCickListener itemCickListener;
+
+    public void setItemCickListener(OnMyItemCickListener itemCickListener) {
+        this.itemCickListener = itemCickListener;
+    }
+    public SimilarAdapter(List<BestDeal> bestDealList) {
+        this.bestDealList = bestDealList;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.viewholder_similar,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        BestDeal bestDeal = bestDealList.get(position);
+        holder.imageView.setImageResource(bestDeal.getImageId());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                itemCickListener.DoSomeThing(position);
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return bestDealList.size();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        private ConstraintLayout cardView;
+        private ImageView imageView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = itemView.findViewById(R.id.constraint1);
+            imageView = itemView.findViewById(R.id.img);
+
+        }
+    }
+}
